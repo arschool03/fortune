d500fb4b690c3fa536bdf7035931701b75349a52
arschool03 <03@arschool.tech>
2018-03-12 07:38:55 +0000

    push test

--- work/app.js
+++ work/app.js
@@ -12,23 +12,51 @@ let shokiSettei = () => {
   kakusuPanel(modoruButton);
 
   // タイトル画面のおみくじ画像を設定する
-  setteiHomeImage('omikuji.png');
+  setteiHomeImage('r.png');
 
 }
 
 // おみくじの中身
 let omikujiNakami = [
-  { // 吉の設定
-    kekkaTitle: '', // おみくじの結果のタイトル文
-    kekkaSetsumei: '', // おみくじの結果につける説明文
-    kekkaImage: '' //おみくじの結果に表示する画像
+  { // 青の設定
+    kekkaTitle: '青', // おみくじの結果のタイトル文
+    kekkaSetsumei: '海や青空をみてみよう', // おみくじの結果につける説明文
+    kekkaImage: 'ao.png' //おみくじの結果に表示する画像
   },
-  { // 凶の設定
-    kekkaTitle: '', // おみくじの結果のタイトル文
-    kekkaSetsumei: '', // おみくじの結果につける説明文
-    kekkaImage: '' //おみくじの結果に表示する画像
-  }
-];
+ { // 赤の設定
+    kekkaTitle: '赤', // おみくじの結果のタイトル文   
+    kekkaSetsumei: '赤いものを見てみよう', // おみくじの結果につける説明文
+    kekkaImage: 'aka.png' //おみくじの結果に表示する画像
+  },
+  { // 黄の設定
+    kekkaTitle: '黄色', // おみくじの結果のタイトル文
+    kekkaSetsumei: '雷は見ないように', // おみくじの結果につける説明文
+    kekkaImage: 'kiiro.png' //おみくじの結果に表示する画像
+  },
+  { // 緑の設定
+    kekkaTitle: '緑', // おみくじの結果のタイトル文   
+    kekkaSetsumei: '木や森などを見よう', // おみくじの結果につける説明文
+    kekkaImage: 'midori.png' //おみくじの結果に表示する画像
+  },
+  { // 緑の設定
+    kekkaTitle: '白', // おみくじの結果のタイトル文   
+    kekkaSetsumei: '雲などを見よう', // おみくじの結果につける説明文
+    kekkaImage: 'shiro.png' //おみくじの結果に表示する画像
+  },
+  { // 緑の設定
+    kekkaTitle: 'オレンジ', // おみくじの結果のタイトル文   
+    kekkaSetsumei: 'みかんなどを見よう', // おみくじの結果につける説明文
+    kekkaImage: 'orenge.png' //おみくじの結果に表示する画像
+  },
+  { // 緑の設定
+    kekkaTitle: '紫', // おみくじの結果のタイトル文   
+    kekkaSetsumei: 'ブドウなどを見よう', // おみくじの結果につける説明文
+    kekkaImage: 'murasaki.png' //おみくじの結果に表示する画像
+  },
+]; 
+
+  
+
 
 // おみくじを引く
 let hikuOmikuji = () => {
@@ -64,3 +92,12 @@ let hikuOmikuji = () => {
 
 // 読み込み終わったら初期設定を実行する
 shokiSettei();
+
+// ルーレットの中身
+let omikujiRoulette = [
+  'ao.png',
+  'aka.png',
+  'kiiro.png',
+  'midori.png',
+  'murasaki.png'
+];
